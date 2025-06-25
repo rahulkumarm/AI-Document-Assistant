e4c42756dfcf195fe2f6d297baa2876c539c6b0e
Rahul Kumar <rahul@macbookair.mynetworksettings.com>
2025-06-25 18:16:20 -0400

    Fix module resolution: inline cn utility function in UI components to eliminate import issues

--- frontend/src/components/ui/textarea.tsx
+++ frontend/src/components/ui/textarea.tsx
@@ -1,6 +1,10 @@
 import * as React from "react"
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-import { cn } from "../../lib/utils"
+function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
 
 const Textarea = React.forwardRef<
   HTMLTextAreaElement,
